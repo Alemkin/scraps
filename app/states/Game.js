c8329aac97bccfd8ae89537a67f98f10e1ac908c
Alexander Lemkin <alexanderlemkin@gmail.com>
2018-06-14 23:09:49 -0400

    CCreated initial project, es6, webpack, and got a dog to move around with arrow keys

---
+++ app/states/Game.js
@@ -0,0 +1,36 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+import Dog from '../sprites/Dog'
+import { DOG } from '../constants/assets'
+
+export default class extends Phaser.State {
+  init() { }
+  preload() { }
+
+  create() {
+    const bannerText = 'Scraps'
+    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
+      font: '40px Bangers',
+      fill: '#77BFA3',
+      smoothed: false
+    })
+
+    banner.padding.set(10, 16)
+    banner.anchor.setTo(0.5)
+
+    this.dog = new Dog({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: DOG
+    })
+
+    this.game.add.existing(this.dog)
+  }
+
+  render() {
+    if (__DEV__) {
+      this.game.debug.spriteInfo(this.dog, 32, 32)
+    }
+  }
+}
